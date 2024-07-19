4ccffb7e463ea484e54d271c7816d938ef27fff3
spookyambassador98 <bakuwork08@gmail.com>
2024-07-19 01:10:34 -0400

    init

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 const port = process.env.PORT || 3333;
-
+const cors = require('cors')
 // Serve static files from the 'public' directory
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Middleware to parse URL-encoded bodies
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(cors())
 
 // Serve the main HTML file
 app.get('/', (req, res) => {
